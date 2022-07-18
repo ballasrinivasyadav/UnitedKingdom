d90cfe6df5416f7d31e882379a757260e9506294
srinivas <ballasrinivasyadav72@gmail.com>
2022-07-18 17:52:38 +0530

    Architect Asian Guy

--- test_fixturesData.py
+++ test_fixturesData.py
@@ -6,18 +6,5 @@ from pytestsDemo.BaseClass import BaseClass
 @pytest.mark.usefixtures("dataLoad")
 class TestExample2(BaseClass):
 
-    def test_editProfile(self, dataLoad):
-        log = self.getLogger()
-        log.info(dataLoad[0])
-        log.info(dataLoad[2])
-        print(dataLoad[2])
-
-############################
-    def test_editProfile(self, dataLoad):
-        log = self.getLogger()
-        log.info(dataLoad[0])
-        log.info(dataLoad[2])
-        print(dataLoad[2])
-
 
 
